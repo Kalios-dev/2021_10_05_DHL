c3d54d411a752d5f2199299ab65ceb5b81badc8f
Kalios-dev <79063054+Kalios-dev@users.noreply.github.com>
2021-10-05 17:27:39 +0200

    Mise en palce d'un fichier JS

---
+++ JS/app.js
@@ -0,0 +1,3 @@
+// document.getElementById('jsLoaded').remove();
+document.getElementById('jsLoaded').style.backgroundcolor="skyblue";
+document.getElementById('jsLoaded').innerHTML="Js bien chargé";
